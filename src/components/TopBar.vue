08c3492391044f5b7c964f8dbe78b1d36734ddd9
Richard Wong <ricwtk@gmail.com>
2025-08-04 14:56:27 +0800

    update primary color

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="w-full bg-primary-500 text-center my-2 p-2 rounded text-white font-bold text-xl">Peer Assessment Form</div>
+  <div class="w-full bg-primary-700 text-center my-2 p-2 rounded text-white font-bold text-xl">Peer Assessment Form</div>
 </template>
