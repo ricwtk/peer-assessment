441b2fcd533427d7c04a8b01323339e28dd1d0f1
Richard Wong <ricwtk@gmail.com>
2025-08-04 16:51:34 +0800

    update rating bar color:

---
+++ src/components/AssessmentDisplay.vue
@@ -0,0 +1,35 @@
+<template>
+  <Panel>
+    <template #header>
+      <Tag v-for="member in props.assessment.members">
+        <div class="flex flex-col">
+          <span>{{ member.name }}</span>
+          <span class="text-xs">{{ member.id }}</span>
+        </div>
+      </Tag>
+    </template>
+    <ProgressBar :value="props.assessment.rating" 
+      :pt="{
+        value: { style: `background: ${barcolor}` }
+      }"></ProgressBar>
+    {{ props.assessment.justification }}
+  </Panel>
+</template>
+
+<script setup>
+const props = defineProps(["assessment"])
+
+import { ref, computed } from 'vue'
+const ratingColorN = 6
+const colors = [
+  "oklch(63.7% 0.237 25.331)",
+  "oklch(70.5% 0.213 47.604)",
+  "oklch(76.9% 0.188 70.08)",
+  "oklch(79.5% 0.184 86.047)",
+  "oklch(76.8% 0.233 130.85)",
+  "oklch(72.3% 0.219 149.579)",
+  "oklch(69.6% 0.17 162.48)"
+]
+console.log(Math.round(props.assessment.rating/100*colors.length))
+const barcolor = computed(() => colors[Math.round(props.assessment.rating/100*colors.length)])
+</script>
